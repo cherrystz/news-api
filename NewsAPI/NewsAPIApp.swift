ebb1fa14ef674d8dbddc73939f02bf835def4c97
cherryst <pharuetthapoljp@gmail.com>
2024-06-20 13:48:23 +0700

    Initial Commit

---
+++ NewsAPI/NewsAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAPIApp.swift
+//  NewsAPI
+//
+//  Created by pharuthapol on 20/6/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct NewsAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
