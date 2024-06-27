62cca9fe2746ed79897ec8411330d200d1384872
cherryst <pharuetthapoljp@gmail.com>
2024-06-27 13:03:11 +0700

    commit structure app

--- NewsAPI/Services/NewsService.swift
+++ NewsAPI/Services/NewsService.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import Combine
+
+class NewsService {
+    private let apiKey = "0d2a24fcc92a47f1ad176e0aed50b567"
+    private let baseUrl = "https://newsapi.org/v2/"
+    
+    func fetchNews(page: Int) -> AnyPublisher<[Article], Error> {
+        guard let url = URL(string: "\(baseUrl)top-headlines?country=us&apiKey=\(apiKey)&page=\(page)") else {
+            return Fail(error: URLError(.badURL))
+                .eraseToAnyPublisher()
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        return URLSession.shared.dataTaskPublisher(for: request)
+            .tryMap { output in
+                guard let response = output.response as? HTTPURLResponse,
+                      response.statusCode == 200 else {
+                    throw URLError(.badServerResponse)
+                }
+                return output.data
+            }
+            .decode(type: NewsResponse.self, decoder: JSONDecoder())
+            .map { $0.articles }
+            .eraseToAnyPublisher()
+    }
+}
+
